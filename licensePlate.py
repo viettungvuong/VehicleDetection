60884ebdfa31883aec4ac52c5969052087727024
viettungvuong <viettungvuong@gmail.com>
2023-04-01 11:03:50 +0700

    Lấy được ảnh biển số xe

--- licensePlate.py
+++ licensePlate.py
@@ -1,51 +1,63 @@
 import cv2
 import imutils
+import os
 import pytesseract
 
-path='license_plates/'
-image = cv2.imread(path+'3.jpeg')
-# chinh kich thuoc hinh xuong 500 px
-image = imutils.resize(image, width=500)
-# tao gray image
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-# giam noise cua anh
-gray_image = cv2.bilateralFilter(gray_image, 11, 17, 17)
+path = "license_plates/"
+sampleImg = cv2.imread(path+'6.jpeg')
 
-# edge detection
-edge = cv2.Canny(gray_image, 30, 200)
-# tim contour
-contours, new = cv2.findContours(edge.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-img1 = image.copy()
-# cv2.drawContours(img1, contours, -1, (0, 255, 0), 3)
-# cv2.imshow("img1",img1)
-# cv2.waitKey()
+def getLicensePlate(image):
+    # chinh kich thuoc hinh xuong 500 px
+    image = imutils.resize(image, width=500)
+    # tao gray image
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # giam noise cua anh
+    gray_image = cv2.bilateralFilter(gray_image, 11, 17, 17)
 
-valid_contours=[]
-# tim nhung contour phu hop, du rong
-for contour in contours:
-    if (cv2.contourArea(contour)>=300):
-        valid_contours.append(contour)
+    # edge detection
+    edge = cv2.Canny(gray_image, 30, 200)
+    # tim contour
+    contours, new = cv2.findContours(edge.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    # cv2.drawContours(img1, contours, -1, (0, 255, 0), 3)
+    # cv2.imshow("img1",img1)
+    # cv2.waitKey()
 
-# cv2.drawContours(img1,valid_contours,-1, (0,255,0), 3)
-# cv2.imshow('img1',img1)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+    valid_contours=[]
+    # tim nhung contour phu hop, du rong
+    for contour in contours:
+       if (cv2.contourArea(contour)>=300):
+          valid_contours.append(contour)
 
-# tim countour nao ma la bien so xe
-for contour in valid_contours:
-    perimeter = cv2.arcLength(contour, True) # chu vi contour
+       # cv2.drawContours(img1,valid_contours,-1, (0,255,0), 3)
+       # cv2.imshow('img1',img1)
+       # cv2.waitKey(0)
+       # cv2.destroyAllWindows()
 
-    # ham nay tra ve polygonal representation cua contour
-    # ham nay con giup contour don gian hon nhung van giu duoc hinh dang
-    approx = cv2.approxPolyDP(contour, 0.018 * perimeter, True)
+       # tim countour nao ma la bien so xe
+       for contour in valid_contours:
+          perimeter = cv2.arcLength(contour, True) # chu vi contour
 
-    if len(approx)==4: # neu co 4 canh
-        shape = approx # tim duoc bien so xe
-        # luu contour chua bien so xe
+          # ham nay tra ve polygonal representation cua contour
+          # ham nay con giup contour don gian hon nhung van giu duoc hinh dang
+          approx = cv2.approxPolyDP(contour, 0.018 * perimeter, True)
 
-        break
+          if len(approx)==4: # neu co 4 canh
+             shape = approx # tim duoc bien so xe
+             # luu contour chua bien so xe
+              
+             # lay phan bien so xe
+             x, y, w, h = cv2.boundingRect(contour)
+             processImg = image [y: y + h, x: x + w]
+             cv2.imwrite(path+'a.png',processImg)
 
-cv2.drawContours(image,[shape], -1, (0, 255, 0), 3)
-cv2.imshow('Detected plate', image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+             cv2.drawContours(image, [shape], -1, (255, 0, 0), 3)
+             cv2.imshow('License plate',image)
+             cv2.waitKey(1)
+             cv2.destroyAllWindows()
+
+             break
+       
+
+getLicensePlate(sampleImg)
+
+
